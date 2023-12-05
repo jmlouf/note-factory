0b617aa60f27035cb03f22fb2d22676dc5231473
joem3847 <joem3847@gmail.com>
2023-12-04 22:11:10 -0600

    Updated comments.

--- middleware/clog.js
+++ middleware/clog.js
@@ -1,4 +1,4 @@
-// Custom middleware that logs to console [c(onsole)log] out the type and path of each request to the server.
+// Logs type and path of request to console.
 const clog = (req, res, next) => {
     const fgCyan = '\x1b[36m';
     switch (req.method) {
